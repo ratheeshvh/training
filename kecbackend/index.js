4ee1bfc8f535f9fd9ec13db9c93ab6b9cabc2443
RATHEESH VH <124667340+ratheeshvh@users.noreply.github.com>
2025-01-06 08:22:58 +0530

    Add files via upload

---
+++ kecbackend/index.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var path = require('path');
+var mdb = require('mongoose');
+var User = require('./module/users');
+var app = express();
+const PORT = 3001;
+
+app.use(express.json());
+
+mdb.connect("mongodb://localhost:27017/").then(() => {
+    console.log("MongoDB connection successful");
+}).catch(() => {
+    console.log("Check your connection String");
+});
+
+app.get('/', (req, res) => {
+    res.send("Welcome to Backend Server");
+});
+
+app.get('/json', (req, res) => {
+    res.json({ server: "Welcome to Backend", url: "localhost", port: PORT });
+});
+
+app.get('/static', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/signup', (req, res) => {
+    console.log(req.body);
+    var { firstName, lastName, email } = req.body;
+    console.log(firstName, lastName, email);
+    try {
+        var newUser = new User({
+            firstName: firstName,
+            lastName: lastName,
+            email: email
+        });
+        newUser.save();
+        console.log("User Added Successfully");
+        res.status(200).send("User Added Successfully");
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Backend Server Started\nUrl:http://localhost:${PORT}`);
+});
